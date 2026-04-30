50690f896dcfac507b35176fc8593c79d60eff29
OscarArise <arisechonova@gmail.com>
2026-04-29 23:53:24 -0600

    Enemy Class

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,36 +2,49 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from enemyShip import Enemy_ship
 
 class Alien_invasion:
     """Clase general para administrar los activos y el comportamiento del juego"""
     def __init__(self):
         """Inicializa el juego y crea recursos del juego"""
         pygame.init()
-        
+        #instancia de settings
         self.settings = Settings()
         
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        
+        #instancia de Ship
         self.ship = Ship(self)
+        #instancia de enemy
+        self.enemy = Enemy_ship(self)
 
         
     def run_game(self):
         """Iniciar el bucle principal del juego"""
         while True:
-            #Este antento a los eventos del mouse y teclado
-            for event in pygame.event.get():
+            #Revisa los eventos del mouse y teclado
+            self.check_events()
+            
+            #Actualizacion de los objetos en pantalla
+            self.update_screen() 
+            
+    def check_events(self):
+         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
                     
-            #Vuelva a dibujar la pantalla durante cada pasa del bucle
-            self.screen.fill(self.settings.bg_color)
-            
-            self.ship.blitme()
-                    
-            #Hacer visible la pantalla dibujada mas reciente
-            pygame.display.flip()
+    def update_screen(self):
+        #Vuelva a dibujar la pantalla durante cada pasa del bucle
+        self.screen.fill(self.settings.bg_color)
+        #Dibuja la nave en cada actualizacion del bucle
+        self.ship.blitme()
+        #Dibuja la nave enemiga
+        self.enemy.enemyblitme()
+        #Actualizar cada pantalla a la version mas reciente
+        pygame.display.flip()
+        
+        
             
 if __name__ == '__main__':
     #Cree una instancia de juego y ejecute el juego
--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ class Settings:
         #Ajuste de la pantalla
         self.screen_width = 1200
         self.screen_height = 700
-        self.bg_color = (230,230,230)
+        self.bg_color = (10, 25, 60)
