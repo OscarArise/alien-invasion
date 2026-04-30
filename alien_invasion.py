e5a900692472342fa58f0100c336158b82ac57a3
OscarArise <arisechonova@gmail.com>
2026-04-30 17:08:49 -0600

    Events refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -38,29 +38,39 @@ class Alien_invasion:
                     sys.exit()
                     
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        #Mueve la nave a la derecha
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        #Mueve la nave a la izquierda
-                        self.ship.moving_left = True
-                        #Mueve la nave hacia arriba
-                    elif event.key == pygame.K_UP:
-                        self.ship.moving_up = True
-                        #mueve la nave hacia abajo
-                    elif event.key == pygame.K_DOWN:
-                        self.ship.moving_down = True
+                    self.check_keydown_events(event)
                 
                 elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False
-                    elif event.key == pygame.K_UP:
-                        self.ship.moving_up = False
-                    elif event.key == pygame.K_DOWN:
-                        self.ship.moving_down = False
-                    
+                    self.check_keyup_events(event)
+                
+    
+    def check_keydown_events(self,event):
+        if event.key == pygame.K_RIGHT:
+            #Mueve la nave a la derecha
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            #Mueve la nave a la izquierda
+            self.ship.moving_left = True
+            #Mueve la nave hacia arriba
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+            #mueve la nave hacia abajo
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+            
+    def check_keyup_events(self,event):
+         if event.key == pygame.K_RIGHT:
+             self.ship.moving_right = False
+         elif event.key == pygame.K_LEFT:
+             self.ship.moving_left = False
+         elif event.key == pygame.K_UP:
+             self.ship.moving_up = False
+         elif event.key == pygame.K_DOWN:
+             self.ship.moving_down = False
+
+
+        
+    
     def update_screen(self):
         #Vuelva a dibujar la pantalla durante cada pasa del bucle
         self.screen.fill(self.settings.bg_color)
